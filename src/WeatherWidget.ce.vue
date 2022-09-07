6defe020aaade8c8c8737edc8d465d6123941895
ismarsel <ismarsel@gmail.com>
2022-09-08 02:27:20 +0500

    fixed the styles error of child components

--- src/WeatherWidget.ce.vue
+++ src/WeatherWidget.ce.vue
@@ -1,26 +1,35 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <button class="btn btn-gear">x</button>
+    <delete-button />
+    <div class="wrapper">
+      <rename-button />
+    </div>
+  </div>
 </template>
 
 <script>
-
-
+import DeleteButton from "./components/UI/DeleteButton.ce.vue";
+import RenameButton from './components/UI/RenameButton.ce.vue';
 export default {
   name: "App",
+  components: { DeleteButton, RenameButton },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+ width: 150px;
+ height: 300px;
+ background-color: #888;
 }
-a {
-  color: #42b983;
+.btn {
+  border: none;
+  outline: none;
+  // padding: 8px;
+  background-color: rgba(136, 136, 136, 0.3);
+  color: #fff;
+  border-radius: 4px;
+  transition: all 0.25s ease;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { defineCustomElement } from 'vue';
+/* import { defineCustomElement } from 'vue';
 import App from './WeatherWidget.ce.vue';
 
 const widget = defineCustomElement(App);
-customElements.define('weather-widget', widget);
+customElements.define('weather-widget', widget); */
+import { defineCustomElementWrapped } from "./defineCustomElementWithStyles";
+import App from "./WeatherWidget.ce.vue";
+
+customElements.define(
+  "weather-widget",
+  defineCustomElementWrapped(App)
+);
